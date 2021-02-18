902e886e1d30c5f72d6996d4cd9bca7c9c5fc82c
xp <xp@CETYSDAM00>
2021-02-18 12:57:48 +0100

    Ejercicio findDuplicate con el algoritmo de Floyd

--- src/ejerciciosnivel02_2021/EjerciciosNivel02_2021.java
+++ src/ejerciciosnivel02_2021/EjerciciosNivel02_2021.java
@@ -51,11 +51,36 @@ public class EjerciciosNivel02_2021 {
         }
         return coste;
     }
-    public void coste1
+    /**
+     * Encuentra el número qu está duplicado en una lista de ints
+     * usando el algoritmo de Floyd
+     * @param listaNumeros
+     * @return 
+     */
+    // [1, 3, 2, 4, 4]
+    public int findDuplicate(int [] listaNumeros){
+        int tortuga = listaNumeros[0];
+        int liebre = listaNumeros[0];
+        boolean iguales = false;
+        while (!iguales){
+            tortuga = listaNumeros[tortuga];
+            liebre = listaNumeros[listaNumeros[liebre]];
+            if (tortuga == liebre){
+                iguales = true;
+            }
+        }
+        int aux1 = listaNumeros[0];
+        int aux2 = tortuga;
+        while (aux1 != aux2){
+            aux1 = listaNumeros[aux1];
+            aux2 = listaNumeros[aux2];
+        }
+        return aux1;
+    }
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main (String[] args){
         EjerciciosNivel02_2021 ejercicio = new EjerciciosNivel02_2021();
         char [][] listaPalabras = {
         {'P', 'A', 'T', 'A'},
@@ -68,7 +93,14 @@ public class EjerciciosNivel02_2021 {
     };
         
         System.out.println(ejercicio.esEscaleraDePalabras(listaPalabras));
-        System.out.println(ejercicio.costeErroresADN('GGGA-GAATCTCTGGACT', 'CCCTACTTA-AGACCGGT'));
+        
+        
+        
+        System.out.println(ejercicio.findDuplicate(new int[] {3,2,3,4,1}));
+        System.out.println(ejercicio.findDuplicate(new int[] {4,2,3,4,1}));
+        System.out.println(ejercicio.findDuplicate(new int[] {1,1,3,4,1}));
+
+        
     }
     
 }
